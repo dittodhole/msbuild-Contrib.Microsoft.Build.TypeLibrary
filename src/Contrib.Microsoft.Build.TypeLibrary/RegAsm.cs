3485547edb14c46e8428b56905d3be88fccc719a
Andreas Niedermair <dev@andreas.niedermair.name>
2020-02-10 15:50:13 +0100

    init

---
+++ src/Contrib.Microsoft.Build.TypeLibrary/RegAsm.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Contrib.Microsoft.Build.TypeLibrary
+{
+  public sealed class RegAsm : ToolTask
+  {
+    [Required]
+    public ITaskItem Assembly { get; set; }
+
+    [Output]
+    public ITaskItem RegFile { get; set; }
+
+    /// <inheritdoc/>
+    protected override string GenerateFullPathToTool()
+    {
+      return ToolLocationHelper.GetPathToDotNetFrameworkFile(this.ToolExe,
+                                                             TargetDotNetFrameworkVersion.VersionLatest,
+                                                             DotNetFrameworkArchitecture.Current);
+    }
+
+    /// <inheritdoc/>
+    protected override string ToolName
+    {
+      get
+      {
+        return "RegAsm.exe";
+      }
+    }
+
+    /// <inheritdoc/>
+    protected override string GenerateCommandLineCommands()
+    {
+      var commandLineBuilder = new CommandLineBuilder();
+
+      commandLineBuilder.AppendFileNameIfNotNull(this.Assembly);
+      commandLineBuilder.AppendSwitchIfNotNull("/regfile:", RegAsm.GetRegFileTaskItem(this.Assembly));
+
+      var result = commandLineBuilder.ToString();
+
+      return result;
+    }
+
+    /// <inheritdoc/>
+    public override bool Execute()
+    {
+      var result = base.Execute();
+      if (result)
+      {
+        this.RegFile = RegAsm.GetRegFileTaskItem(this.Assembly);
+      }
+
+      return result;
+    }
+
+    private static ITaskItem GetRegFileTaskItem(ITaskItem assembly)
+    {
+      var result = new TaskItem(assembly)
+                   {
+                     ItemSpec = System.IO.Path.ChangeExtension(assembly.ItemSpec, ".reg")
+                   };
+
+      return result;
+    }
+  }
+}
---
+++ src/Contrib.Microsoft.Build.TypeLibrary/TlbExp.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Contrib.Microsoft.Build.TypeLibrary
+{
+  public sealed class TlbExp : ToolTask
+  {
+    [Required]
+    public ITaskItem Assembly { get; set; }
+
+    [Output]
+    public ITaskItem TlbFile { get; set; }
+
+    /// <inheritdoc/>
+    protected override string GenerateFullPathToTool()
+    {
+      return ToolLocationHelper.GetPathToDotNetFrameworkSdkFile(this.ToolExe,
+                                                                TargetDotNetFrameworkVersion.VersionLatest,
+                                                                DotNetFrameworkArchitecture.Current);
+    }
+
+    /// <inheritdoc/>
+    protected override string ToolName
+    {
+      get
+      {
+        return "TlbExp.exe";
+      }
+    }
+
+    /// <inheritdoc/>
+    protected override string GenerateCommandLineCommands()
+    {
+      var commandLineBuilder = new CommandLineBuilder();
+
+      commandLineBuilder.AppendFileNameIfNotNull(this.Assembly);
+      commandLineBuilder.AppendSwitchIfNotNull("/out:", TlbExp.GetTlbFileTaskItem(this.Assembly));
+
+      var result = commandLineBuilder.ToString();
+
+      return result;
+    }
+
+    /// <inheritdoc/>
+    public override bool Execute()
+    {
+      var result = base.Execute();
+      if (result)
+      {
+        this.TlbFile = TlbExp.GetTlbFileTaskItem(this.Assembly);
+      }
+
+      return result;
+    }
+
+    private static ITaskItem GetTlbFileTaskItem(ITaskItem assembly)
+    {
+      var result = new TaskItem(assembly)
+                   {
+                     ItemSpec = System.IO.Path.ChangeExtension(assembly.ItemSpec, ".tlb")
+                   };
+
+      return result;
+    }
+  }
+}
